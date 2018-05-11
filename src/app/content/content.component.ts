14ac41d20738a411588142762caa2c734a6578a1
at-loantruong <loan.truong@asiantech.vn>
2018-05-11 15:39:04 +0700

    fx bugs space

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
+
 export class ContentComponent {
 
   appList: any[] = [ 
-  {
-    "id": "1",
-    "title": "Coffee sugar, chicory seasonal espresso barista americano",
-    "src": '../assets/images/image-1.png',
-    "content": 'Arista, percolator, cream, aromatic, fair trade, breve body instant lungo blue mountain cappuccino. Americano aroma mug espresso latte crema milk redeye acerbic.'
-  },
-  {
-    "id": "2",
-    "title": "Wherever far wow thus a squirrel raccoon jeez jaguar this from along",
-    "src": '../assets/images/image-2.png',
-    "content": 'Arista, percolator, cream, aromatic, fair trade, breve body instant lungo blue mountain cappuccino. Americano aroma mug espresso latte crema milk redeye acerbic.'
-  }
+    {
+      "id": '1',
+      "title": 'Coffee sugar, chicory seasonal espresso barista americano',
+      "src": '../assets/images/image-1.png',
+      "content": 'Arista, percolator, cream, aromatic, fair trade, breve body instant lungo blue mountain cappuccino. Americano aroma mug espresso latte crema milk redeye acerbic.'
+    },
+    {
+      "id": '2',
+      "title": 'Wherever far wow thus a squirrel raccoon jeez jaguar this from along',
+      "src": '../assets/images/image-2.png',
+      "content": 'Arista, percolator, cream, aromatic, fair trade, breve body instant lungo blue mountain cappuccino. Americano aroma mug espresso latte crema milk redeye acerbic.'
+    }
   ];
 
   selectedPost = {}
@@ -28,5 +30,5 @@ export class ContentComponent {
     this.selectedPost = post;
     this.isSelected = true;
   }
-
+  
 }
